72a0ca68110a0919430154a8e01c911dd80bbda6
Aziz Ben Lamine <azizbenlamine@gmail.com>
2026-01-10 14:28:00 +0100

    Time: 27 ms (5.94%) | Memory: 46 MB (50.23%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        Map<Character, Integer> charS = new HashMap<>();
+        Map<Character, Integer> charT = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            charS.put(s.charAt(i), charS.getOrDefault(s.charAt(i), 0) + 1);
+            charT.put(t.charAt(i), charT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        return charS.equals(charT);
+    }
+}
