fd0f819f9ee8d583b7895008e9d2212c4660b8c7
Aziz Ben Lamine <azizbenlamine@gmail.com>
2026-03-08 15:01:46 +0100

    Time: 420 ms (43.65%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT DISTINCT author_id id FROM VIEWS where author_id=viewer_id ORDER BY id ASC;
+SELECT DISTINCT author_id id FROM VIEWS where author_id=viewer_id ORDER BY id;
